c3a9f7db6b406fa7b2779e986e7fc78fb95fe3f1
Adrian Kałuziński <kaluza@gmail.com>
2020-10-19 12:09:15 +0200

    Create Notes and Recipie entities

---
+++ src/main/java/eu/kaluzinski/recipies/RecipiesApplication.java
@@ -0,0 +1,13 @@
+package eu.kaluzinski.recipies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/kaluzinski/recipies/RecipiesApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.kaluzinski.recipies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
