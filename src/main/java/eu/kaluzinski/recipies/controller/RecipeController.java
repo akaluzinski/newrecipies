28b781fe0ed3a98f54c0f5c31f9d77c3d8d5bd2c
Adrian Kałuziński <kaluza@gmail.com>
2020-10-29 21:48:52 +0100

    Replace ResponseStatus with ExceptionHandler

--- src/main/java/eu/kaluzinski/recipies/controller/RecipeController.java
+++ src/main/java/eu/kaluzinski/recipies/controller/RecipeController.java
@@ -1,10 +1,13 @@
 package eu.kaluzinski.recipies.controller;
 
+import eu.kaluzinski.recipies.exceptions.NotFoundException;
 import eu.kaluzinski.recipies.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RecipeController {
@@ -19,5 +22,10 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.getRecipeById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFoundException() {
+        return new ModelAndView("404error");
     }
 }
