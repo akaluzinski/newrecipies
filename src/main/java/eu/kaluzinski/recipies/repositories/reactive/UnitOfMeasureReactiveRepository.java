04054769ba3c415e380c236493e8039a6cd6b8f5
Adrian Kałuziński <kaluza@gmail.com>
2020-11-08 20:19:18 +0100

    Remove ReactiveCrudRepository

--- src/main/java/eu/kaluzinski/recipies/repositories/reactive/UnitOfMeasureReactiveRepository.java
+++
@@ -1,7 +0,0 @@
-package eu.kaluzinski.recipies.repositories.reactive;
-
-import eu.kaluzinski.recipies.model.UnitOfMeasure;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface UnitOfMeasureReactiveRepository extends ReactiveCrudRepository<UnitOfMeasure, Long> {
-}
